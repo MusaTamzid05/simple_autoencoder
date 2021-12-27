30f19bd3776116f7aebfefda009c1c068bce158e
MusaTamzid05 <tamzid101@gmail.com>
2021-12-27 15:15:35 +0600

    base class for autoencoder.

---
+++ lib/autoencoder.py
@@ -0,0 +1,17 @@
+
+
+class Autoencoder:
+    def __init__(self):
+        raise RuntimeError("Need to be implemented")
+
+    def load(self, model_dir_path):
+        raise RuntimeError("Need to be implemented")
+
+    def fit(self, x, y, epochs = 100, batch_size = 32):
+        raise RuntimeError("Need to be implemented")
+
+
+    def save(self, model_dir_path):
+        raise RuntimeError("Need to be implemented")
+
+
