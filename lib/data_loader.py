1405ca340b328336f09f3052a5db3737295e8075
MusaTamzid05 <tamzid101@gmail.com>
2021-12-27 19:09:45 +0600

    catdog example added.

--- lib/data_loader.py
+++ lib/data_loader.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.datasets import fashion_mnist
+import pickle
 
 class DataLoader:
     def __init__(self):
@@ -17,6 +18,34 @@ class FashionMNISTLoader(DataLoader):
     def __init__(self):
         super().__init__()
         (self.x_train, _), (self.x_test, _) = fashion_mnist.load_data()
+
+
+    def process(self, x_data):
+        x_data = x_data.astype("float32") / 255.0
+        x_data = x_data.reshape(x_data.shape[0], -1)
+
+        return x_data
+
+
+    def fit(self):
+        self.x_train = self.process(x_data = self.x_train)
+        self.x_test = self.process(x_data = self.x_test)
+
+        return self.x_train, self.x_test
+
+
+
+
+
+class CatDogDataLoader(DataLoader):
+    def __init__(self):
+        super().__init__()
+
+        with open("./custom_data/data.pickle", "rb") as f:
+            data = pickle.load(f)
+            self.x_train = data["train"]
+            self.x_test = data["test"]
+
 
 
     def process(self, x_data):
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from lib.data_loader import FashionMNISTLoader
+from lib.data_loader import CatDogDataLoader
 from lib.autoencoder import SimpleAutoencoder
 
 from lib.plotter import plot_original_vs_generated
 
-def main():
+def fashion_example():
     loader = FashionMNISTLoader()
     x_train, x_test = loader.fit()
 
@@ -23,5 +24,33 @@ def main():
 
     plot_original_vs_generated(x_test, predictions)
 
+
+def cat_dog_example():
+    loader = CatDogDataLoader()
+    x_train, x_test = loader.fit()
+
+    print(x_train.shape)
+    print(x_test.shape)
+
+
+    autoencoder = SimpleAutoencoder()
+    autoencoder.fit(x = x_train, y = x_test, epochs = 10)
+
+    predictions = autoencoder.predict(x_test)
+
+    original_shape = (x_test.shape[0], 28, 28)
+
+    x_test = x_test.reshape(original_shape)
+    predictions = predictions.reshape(original_shape)
+
+    print(x_test.shape)
+    print(predictions.shape)
+
+    plot_original_vs_generated(x_test, predictions)
+
+
+
+
+
 if __name__ == "__main__":
-    main()
+    cat_dog_example()
